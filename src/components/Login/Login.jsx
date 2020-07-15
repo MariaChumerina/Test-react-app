ba71aca19c493355605294b72b14f74c42698557
MariaChumerina <masha.chumerina@gmail.com>
2020-07-15 16:15:11 +0500

    layout login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Login.css';
 
 export default function Login() {
   return (
-    <div>I'm login component</div>
+    <div className="container margin-top-50">
+      <form className="form">
+        <label htmlFor="userName"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="userName" required/>
+
+        <label htmlFor="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required/>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 }
